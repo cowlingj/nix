f2f27925e4b1ec5c9077aeefbe98dc4e02546319
cowlingj <19248800+cowlingj@users.noreply.github.com>
2025-09-13 13:28:02 +0100

    Add ventoy

--- home-manager/users/base/default.nix
+++ home-manager/users/base/default.nix
@@ -3,10 +3,14 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
+      permittedInsecurePackages = [
+        "ventoy-1.1.07"
+      ];
     };
   };
   home.stateVersion = "23.11";
   home.packages = with pkgs; [
     git-crypt
+    ventoy
   ];
 }
