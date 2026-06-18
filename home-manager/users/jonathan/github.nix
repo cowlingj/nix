e3d00d597a291bddca9ea9f707437a39983fb557
cowlingj <19248800+cowlingj@users.noreply.github.com>
2026-06-18 14:26:07 +0100

    Add foundry zip

--- home-manager/users/jonathan/github.nix
+++ home-manager/users/jonathan/github.nix
@@ -1,26 +1,19 @@
-let
-  github_ssh_key = "/.ssh/github_ed25519";
-in
-{ config, ... }:   {
-  home.file.github_ssh_key = {
-    enable = false;
-    source = "${config.home.homeDirectory}/${github_ssh_key}";
-    target = "${config.home.homeDirectory}/${github_ssh_key}";
-  };
+{
   programs.git = {
     enable = true;
+    lfs.enable = true;
     settings = {
       user.name  = "cowlingj";
       user.email = "19248800+cowlingj@users.noreply.github.com";
       gpg.format = "ssh";
-      # user.signingkey = config.home.file.github_ssh_key.target;
       push = { autoSetupRemote = true; };
       init.defaultBranch = "main";
-      filter.lfs.clean = "git-lfs clean -- %f";
+      "lfs \"extension.git-crypt\"" = {
+        clean = "git-crypt clean";
+      smudge = "git-crypt smudge";
+      priority = 0;
+      };
+      "lfs \"extension.git-crypt.diff\"".textconv = "git-crypt diff";
     };
-  };
-  programs.ssh.matchBlocks.github = {
-    host = "github.com";
-    identityFile = config.home.file.github_ssh_key.target;
   };
 }
